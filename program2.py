1039a35e399ac3ea4232ba898af26114ac28c082
arshaaswathy6-blip <arshaaswathy6@gmail.com>
2025-11-26 11:28:24 +0530

    added flower image in assets folder,in program2 change flower.jpeg to flower.jpg

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-img=cv2.imread("./assets/flower.jpeg")
+img=cv2.imread("./assets/flower.jpg")
 img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 filter={"Original":img,
         "smoothed":cv2.filter2D(img,-1,np.ones((3,3),np.float32)/9),
