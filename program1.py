31fb9c66df2efa874122384988b49bd771f14762
DR-AT0M1C\ANANDHU <anandhu2suresh797@gmail.com>
2025-11-26 11:23:07 +0530

    fixed path issues with p1 and p2

--- program1.py
+++ program1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img=cv.imread("E:\S3 MSc CS\DIP LAB\cat.jpg",0)
+img=cv.imread("./assets/cat.jpg",0)
 equ=cv.equalizeHist(img)
 res=np.hstack((img,equ))
-cv.imwrite('res.png',res)
+cv.imwrite('./output/program1.png',res)
