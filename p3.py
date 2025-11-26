c55f92ffec033208e6539f75267813c07a06cadb
DR-AT0M1C\ANANDHU <anandhu2suresh797@gmail.com>
2025-11-26 11:09:28 +0530

    fixed issues with image extension on p3 and p4 and confirmed out put

--- p3.py
+++ p3.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-img = cv2.imread("./assets/cat.jpeg")
-img_gray = cv2.imread("cat.jpeg",0)
+img = cv2.imread("./assets/cat.jpg")
+img_gray = cv2.imread("./assets/cat.jpg",0)
 img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 smoothed = cv2.blur(img_gray, (3,3))   
 sharpened = cv2.filter2D(img_gray, -1, np.array([[0,-1,0],[-1,5,-1],[0,-1,0]]))
