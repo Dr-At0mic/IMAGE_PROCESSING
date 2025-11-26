ffaf839fc0999b6e06b6cc55484e260cbac7bfe7
DR-AT0M1C\ANANDHU <anandhu2suresh797@gmail.com>
2025-11-26 10:54:35 +0530

    added p3 and p4

---
+++ p3.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv2.imread("cat.jpeg")
+img_gray = cv2.imread("cat.jpeg",0)
+img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+smoothed = cv2.blur(img_gray, (3,3))   
+sharpened = cv2.filter2D(img_gray, -1, np.array([[0,-1,0],[-1,5,-1],[0,-1,0]]))
+titles = ["Original", "Smoothed", "Sharpened"]
+images = [img, smoothed, sharpened]
+for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.imshow(images[i], cmap="gray")
+    plt.title(titles[i])
+    plt.axis("off")
+plt.show()
---
+++ p4.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img=cv2.imread("/assets/cat.jpeg")
+img_rgb =cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+f=np.fft.fft2(gray)
+fshift= np.fft.fftshift(f)
+magnitude_spectrum= 20* np.log(np.abs(fshift)+1)
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.title("Original image (color)")
+plt.imshow(img_rgb)
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.title("Magnitude spectrum(Grayscale)")
+plt.imshow(magnitude_spectrum, cmap="gray")
+plt.axis("off")
+plt.show()
